4bb9b6797491a4536faa95ec339bb7f5d00ef048
Acheme1 <christmichael970@gmail.com>
2022-10-15 09:41:06 -0700

    zero-level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
 	/*
 	 * Main: Entry
 	 * Return: Always 0
 	 */
-
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n);
 
 		return 0;
 }
