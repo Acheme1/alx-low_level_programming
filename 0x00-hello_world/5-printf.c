c2ee4f7c087a1c2334ce83bd7d96270f8555bbc2
Acheme1 <christmichael970@gmail.com>
2022-10-17 13:27:52 -0700

    c programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
 /**
- * Main - print a line of program using printf
+ * main - print a C program using printf
+ * Return: always 0 (success)
  *
- * Return: Always 0 (success)
- */
-int main(void);
+ *
+*/
 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
