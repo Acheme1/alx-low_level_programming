6786b5c0cfa23ca19c5b5cb7573bbb79da4f60ce
Acheme1 <christmichael970@gmail.com>
2022-10-18 21:33:05 -0700

    funtion_nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - _putchar print_alphabet a-z
+ *
+*/
+int void print_alphabet(void)
+{
+
+	char la;
+
+	for (la ='a'; la <='z'; la++);
+		_putchar(la);
+
+	_putchar('\n');
+	
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabet_x10 in lowercase
+ * REturn 1 on success
+ */
+
+
+void print_alphabet_x10(void)
+{
+
+	for (ten = 0; ten < = 9; ten++)
+	{
+		for ('la = 'a'; la <= 'z'; la++')
+			_putchar (la);
+
+		_putchar ('\n');
+	}
+}
