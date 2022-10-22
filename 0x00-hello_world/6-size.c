736c0515a8be09e87141d253278eed61351a7052
Acheme1 <christmichael970@gmail.com>
2022-10-22 04:52:31 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,13 @@ int main(void)
 	int i;
 	char c;
 	double d;
+	long long l;
 	float f;
 
 	printf("%lu\n", sizeof(i));
 	printf("%lu\n", sizeof(c));
 	printf("%lu\n", sizeof(d));
+	printf("%lu\n", sizeof(l));
 	printf("%lu\n", sizeof(f));
 	return (0);
 }
