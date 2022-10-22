9134acfbdf7daef9462779c3e92e164d3f2f4513
Acheme1 <christmichael970@gmail.com>
2022-10-22 05:56:20 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 /**
  * main - prints sizes of computers
  * Return: always 0 (success)
- *
 */
 int main(void)
 {
@@ -14,7 +13,7 @@ int main(void)
 
 	printf("%lu Size of a char: 1 byte(s)\n", sizeof(c));
 	printf("%lu Size of an int: 4 byte(s)\n", sizeof(i));
-	printf("%lu Size of a long int: 4 byte(s)\n", sizeof(l));
+	printf("%lu Size of a long int: 8 byte(s)\n", sizeof(l));
 	printf("%lu Size of a long long: 8 byte(s)\n", sizeof(d));
 	printf("%lu Size of a float: 4 byte(s)\n", sizeof(f));
 	return (0);
