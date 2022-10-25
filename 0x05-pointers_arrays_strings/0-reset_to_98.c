923dc2e5119ca61229497170fda81d54dc0ac04d
Acheme1 <christmichael970@gmail.com>
2022-10-25 02:20:16 +0100

    function that return a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - tak a pointer and uptdate it to 98
+ * @n: pointer
+ *
+ * Return: 0 Always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
