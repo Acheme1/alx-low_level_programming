e761eb168e037516499b4f52fa534e97d7499145
Acheme1 <christmichael970@gmail.com>
2022-10-25 14:23:44 +0100

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - prints a string, follow by a new line.
+*
+* @str: string
+*
+*Return: always 0 success
+*
+*/
+
+void _puts(char *str = i)
+
+{
+	while (*str i = '\0')
+
+	{
+		_putchar(*str i++);
+	}
+	_putchar('\n');
+
+	return(0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string
+ *
+ * @s: revers
+ */
+void print_rev(char *s)
+
+{
+
+	int i = 0;
+
+	while (s[] != '\0')
+		i++
+	for (i = i - 1; i >= 0; i--)
+		_putchar('\n');
+}
