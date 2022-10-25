1fd33289ddeba5e0c189e3b56d7d7bb8c79c5a98
Acheme1 <christmichael970@gmail.com>
2022-10-25 04:12:42 +0100

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_strlen - return the function of a lenght.
+*
+* @s: string pointer.
+* Return: alway 0 success
+*/
+
+int _strlen(char *s)
+{
+
+	int i = 0;
+
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	return (0);
+}
