8e4f00f67c48814f73795b6eb029c2f6d31893ad
Acheme1 <christmichael970@gmail.com>
2022-11-03 04:12:55 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,21 @@
 #include "main.h"
+w
+int is_prime(unsigned int n, unsigned int c)
+ 11 {
+ 12         if (n % c == 0)
+ 13         {
+ 14         if (n == c)
+ 15         return (1);
+ 16         else
+ 17         return (0);
+ 18         }
+ 19         return (0 + is_prime(n, c + 1));
+ 20 }
 
 /**
 * is_prime_number - detects if number is a prime number or not
 * @n: input
+* @c: iteration
 *Return: 1
 */
 int is_prime_number(int n)
